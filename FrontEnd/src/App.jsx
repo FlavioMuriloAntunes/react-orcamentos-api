bedd9e4de922ade956118c48c87b8c86e20effd9
Flavio Antunes <flavioantunes457@gmail.com>
2025-11-01 19:46:11 -0300

    Adicionando FrontEnd

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+       <h1>Ola todos </h1>
+       <h2>Momo so me charopeira</h2>
+      </div>
+
+      
+    </>
+  )
+}
+
+export default App
